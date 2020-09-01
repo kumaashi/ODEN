c5a306ad5bed2757eb034ec70c78f8d809025966
lute-gyabo-yasai <gyaboyan@gmail.com>
2020-09-01 23:17:25 +0900

    added luabind for vulkan env

--- Source/lmake.bat
+++ Source/lmake.bat
@@ -1,3 +1,4 @@
 cl /nologo /Ox /EHsc /GS- oden_lua.cpp oden_util.cpp user32.lib gdi32.lib lua.lib dx12_oden.cpp /Fe:oden_lua_dx12.exe
 
 cl /nologo /Ox /EHsc /GS- oden_lua.cpp oden_util.cpp user32.lib gdi32.lib lua.lib dx11_oden.cpp /Fe:oden_lua_dx11.exe
+cl /nologo /Ox /EHsc /GS- oden_lua.cpp oden_util.cpp user32.lib gdi32.lib lua.lib vk_oden.cpp /Fe:oden_lua_vk.exe
