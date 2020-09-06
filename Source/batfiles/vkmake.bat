bac0e128219b2b026003c892146285fdb2299c8b
lute-gyabo-yasai <gyaboyan@gmail.com>
2020-09-07 00:09:56 +0900

    move batch file

---
+++ Source/batfiles/vkmake.bat
@@ -0,0 +1,1 @@
+cl /nologo /Ox /EHsc /GS- /std:c++latest vk_oden.cpp oden_util.cpp sample_code.cpp /IC:\VulkanSDK\1.2.148.1\Include
--- Source/vkmake.bat
+++
@@ -1,2 +0,0 @@
-cl /nologo /Ox /EHsc /GS- /std:c++latest vk_oden.cpp oden_util.cpp sample_code.cpp /IC:\VulkanSDK\1.2.148.1\Include
-REM cl /nologo /Ox /EHsc /GS- /std:c++latest vk_oden.cpp oden_util.cpp  /IC:\VulkanSDK\1.2.148.1\Include sample_code_000.cpp
